3fc8f59c2d4fd6e4064ebb33ec454b76914235bd
Norvik Alexian <norvik@ucraft.com>
2025-02-23 22:32:58 +0400

    added agent to gather user information

--- assistant.py
+++ assistant.py
@@ -1,14 +1,17 @@
 import os
-from typing import Sequence
+from typing import Optional, Sequence
 
 from dotenv import load_dotenv
 from langgraph.graph import START, StateGraph
 from typing_extensions import Annotated, TypedDict
+from langchain_core.tools import tool
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import BaseMessage, trim_messages
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
+
+import llms_constants
 
 load_dotenv()
 
@@ -28,10 +31,7 @@ class State(TypedDict):
 
 prompt_template = ChatPromptTemplate.from_messages(
     [
-        (
-            "system",
-            "You are an Helpful real estate company customer support assistant, that assists user to buy their dream home, and your name is Hoory. Assist the user in their own language: {language}",
-        ),
+        ("system", llms_constants.system_prompt),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
@@ -39,10 +39,35 @@ prompt_template = ChatPromptTemplate.from_messages(
 workflow = StateGraph(state_schema=State)
 
 
+@tool
+def search_house(price: str, location: str, square_meter: str, rooms_amount: int, name: Optional[str]) -> str:
+    """
+    Search for the house in the database to suggest to the user that is looking for house
+    :param price: exact or price range
+    :param location: location of the house
+    :param square_meter: square meter of the house
+    :param rooms_amount: number of rooms
+    :param name: name of the user
+    :return: structured format of the information to call API
+    """
+    request_info = {
+        "price": price,
+        "location": location,
+        "square_meter": square_meter,
+        "rooms": rooms_amount,
+        "name": name,
+    }
+
+    return f"this is your details: {request_info}"
+
+
+model_with_tools = model.bind_tools([search_house])
+
+
 def call_model(state: State):
     trimmed_messages = trimmer.invoke(state["messages"])
     prompt = prompt_template.invoke({"messages": trimmed_messages, "language": state["language"]})
-    response = model.invoke(prompt)
+    response = model_with_tools.invoke(prompt)
     return {"messages": response}
 
 
