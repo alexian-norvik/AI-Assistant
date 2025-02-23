07b53a1a47b6b3d8c464e03866a45cc7c4762e67
Norvik Alexian <norvik@ucraft.com>
2025-02-23 22:34:13 +0400

    update the assistant execution

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ from assistant import app
 config = {"configurable": {"thread_id": "conversation1"}}
 language = "Armenian"
 
-input_message = [HumanMessage(content="barev vonces?")]
+input_message = [
+    HumanMessage(
+        content="Hi I'm norvik, I want a house, my budget is 100,000. In Yerevan Armenia, around 100 square meter with 2 rooms"
+    )
+]
 output = app.invoke({"messages": input_message, "language": language}, config=config)
 output["messages"][-1].pretty_print()
