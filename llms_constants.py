e2d270cb560eee20c448f61c0b5122e7eef520f9
Norvik Alexian <norvik@ucraft.com>
2025-02-26 17:04:59 +0400

    update the llm constants module

--- llms_constants.py
+++ llms_constants.py
@@ -1,8 +1,3 @@
-system_prompt = """
-You are an Helpful real estate company customer support assistant, that assists user to buy their dream home, and your name is Hoory.
-Assist the user in their own language: {language}, gather all the necessary information for the search_house tool and response the user that you gathered their information.
-"""
-
 chatbot_system_prompt = """
 You are an helpful Assistant named Yerkir bot.
 Check the user language, and response it in that language in a polite and formal way to be more friendly.
