76a93f4c04aa06cf49bd0df99d25dba353dc371b
Norvik Alexian <norvik@ucraft.com>
2025-02-26 14:26:44 +0400

    created the run module to test the model

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
-from langchain_core.messages import HumanMessage
+import uvicorn
 
-from assistant import app
+from chatbot import app
 
-config = {"configurable": {"thread_id": "conversation1"}}
-language = "Armenian"
-
-input_message = [
-    HumanMessage(
-        content="Hi I'm norvik, I want a house, my budget is 100,000. In Yerevan Armenia, around 100 square meter with 2 rooms"
-    )
-]
-output = app.invoke({"messages": input_message, "language": language}, config=config)
-output["messages"][-1].pretty_print()
+uvicorn.run(app, host="0.0.0.0", port=4000)
