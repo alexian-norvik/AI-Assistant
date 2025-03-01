91ec12b4a5eb31c6ed108281047487f195556130
Norvik Alexian <norvik@ucraft.com>
2025-03-02 00:03:04 +0400

    created app module to add rest api layer on the chatbot

---
+++ app.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException
+
+from common import schemas, llms_constants
+from services.assistant import chatbot
+
+app = FastAPI()
+
+
+@app.post("/chat", response_model=schemas.ChatResponse)
+def chat(request: schemas.ChatRequest):
+    try:
+        chat_history = [message.to_langchain() for message in request.chat_history]
+
+        result = chatbot(query=request.query, language=request.language, name=request.name, chat_history=chat_history)
+
+        # update chat history
+        conversation_history = request.chat_history.copy()
+        conversation_history.append(schemas.Message(message_type=llms_constants.HUMAN_MSG, content=request.query))
+        conversation_history.append(schemas.Message(message_type=llms_constants.AI_MSG, content=result))
+
+        return schemas.ChatResponse(response=result, chat_history=conversation_history)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
