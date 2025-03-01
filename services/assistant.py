3deb48a1b87d6748d76105887ecc81a7f54b0cf7
Norvik Alexian <norvik@ucraft.com>
2025-03-01 21:36:42 +0400

    added agent to the flow of the chatbot

--- services/assistant.py
+++ services/assistant.py
@@ -1,11 +1,26 @@
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 from common import llms_constants
 from config import OPENAI_API_KEY
 
 if not OPENAI_API_KEY:
     raise EnvironmentError("Setup OpenAI key as your environment variable")
+
+
+@tool
+def house_search(location: str) -> bool:
+    """
+    Check if in the provided location there is any house for sale or not.
+    :param location: Provided location by the user.
+    :return: True or False whether any house is available or not.
+    """
+    if location in ["Yerevan", "Gyumri", "Spitak"]:
+        return True
+    else:
+        return False
 
 
 def chat(query: str, language: str, name: str) -> str:
@@ -22,11 +37,18 @@ def chat(query: str, language: str, name: str) -> str:
         max_retries=llms_constants.MODEL_MAX_RETRIES,
         api_key=OPENAI_API_KEY,
     )
+    tools = [house_search]
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", llms_constants.SYSTEM_PROMPT),
+            ("user", "{input}"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
 
-    prompt = ChatPromptTemplate.from_messages([("system", llms_constants.SYSTEM_PROMPT), ("user", "{input}")])
+    agent = create_tool_calling_agent(llm=llm, tools=tools, prompt=prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-    chain = prompt | llm
+    response = agent_executor.invoke({"language": language, "name": name, "input": query})
 
-    response = chain.invoke({"language": language, "name": name, "input": query}).content
-
-    return response
+    return response["output"]
