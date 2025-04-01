06548a0d3755c9a258636ae6a6a2bd7d3427f7b8
norvik <ai.norvik@gmail.com>
2025-04-01 13:00:26 +0400

    created new endpoint for anthropic model instance, to chat with the user

--- app.py
+++ app.py
@@ -2,20 +2,45 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 
 from common import schemas, llms_constants
-from services.openai_assistant import chatbot
+from services.openai_assistant import openai_chatbot
+from services.anthropic_assistant import anthropic_chatbot
 
 app = FastAPI()
 
 
-@app.post("/chat", response_model=schemas.ChatResponse)
-def chat(request: schemas.ChatRequest):
+@app.post("/openai_chat", response_model=schemas.ChatResponse)
+def openai_chat(request: schemas.ChatRequest):
     try:
         chat_history = [message.to_langchain() for message in request.chat_history]
 
         if len(chat_history) > 15:
             chat_history = chat_history[-15:]
 
-        result = chatbot(query=request.query, language=request.language, name=request.name, chat_history=chat_history)
+        result = openai_chatbot(
+            query=request.query, language=request.language, name=request.name, chat_history=chat_history
+        )
+
+        # update chat history
+        conversation_history = request.chat_history.copy()
+        conversation_history.append(schemas.Message(message_type=llms_constants.HUMAN_MSG, content=request.query))
+        conversation_history.append(schemas.Message(message_type=llms_constants.AI_MSG, content=result))
+
+        return schemas.ChatResponse(response=result, chat_history=conversation_history, language=request.language)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+
+@app.post("/anthropic_chat", response_model=schemas.ChatResponse)
+def anthropic_chat(request: schemas.ChatRequest):
+    try:
+        chat_history = [message.to_langchain() for message in request.chat_history]
+
+        if len(chat_history) > 15:
+            chat_history = chat_history[-15:]
+
+        result = anthropic_chatbot(
+            query=request.query, language=request.language, name=request.name, chat_history=chat_history
+        )
 
         # update chat history
         conversation_history = request.chat_history.copy()
