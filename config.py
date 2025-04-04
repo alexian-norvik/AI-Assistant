1a9e22fc43afa58ad43cbb4047887400707155d9
norvik <ai.norvik@gmail.com>
2025-04-04 18:12:06 +0400

    defined an async anthropic model instance

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
 from dotenv import load_dotenv
+from anthropic import AsyncAnthropic
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+
+ANTHROPIC_MODEL = AsyncAnthropic(api_key=ANTHROPIC_API_KEY)
