6ddcfefd7b703aa7b54562aa85bc49327198cfe2
norvik <ai.norvik@gmail.com>
2025-04-09 17:38:43 +0400

    major changes in the anthropic chat function

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import uvicorn
+import tiktoken
 from loguru import logger
 from fastapi import FastAPI, HTTPException
 
 from common import schemas, llms_constants
+from services import anthropic_assistant
 from services.openai_assistant import openai_chatbot
-from services.anthropic_assistant import anthropic_chatbot
 
 app = FastAPI()
 
@@ -34,21 +35,30 @@ def openai_chat(request: schemas.ChatRequest):
 @app.post("/anthropic_chat", response_model=schemas.ChatResponse)
 async def anthropic_chat(request: schemas.ChatRequest):
     try:
+        user_query = request.query
         chat_history = [message.to_langchain() for message in request.chat_history]
 
         if len(chat_history) > 15:
             chat_history = chat_history[-15:]
 
-        result = await anthropic_chatbot(
-            query=request.query, language=request.language, name=request.name, chat_history=chat_history
+        query_encoding = tiktoken.get_encoding(llms_constants.TIKTOKEN_MODEL)
+        tokens = query_encoding.encode(user_query)
+
+        if len(tokens) > 30:
+            user_query = anthropic_assistant.summarize_query(query=user_query)
+
+        chat_response = await anthropic_assistant.anthropic_chatbot(
+            query=user_query, language=request.language, name=request.name, chat_history=chat_history
         )
 
         # update chat history
         conversation_history = request.chat_history.copy()
-        conversation_history.append(schemas.Message(message_type=llms_constants.HUMAN_MSG, content=request.query))
-        conversation_history.append(schemas.Message(message_type=llms_constants.AI_MSG, content=result))
+        conversation_history.append(schemas.Message(message_type=llms_constants.HUMAN_MSG, content=user_query))
+        conversation_history.append(schemas.Message(message_type=llms_constants.AI_MSG, content=chat_response))
 
-        return schemas.ChatResponse(response=result, chat_history=conversation_history, language=request.language)
+        return schemas.ChatResponse(
+            response=chat_response, chat_history=conversation_history, language=request.language
+        )
     except Exception as e:
         logger.error(f"Failed to generate user response, error message: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail=f"Error: {e}")
