b3e361b540f8a1132ebcc364c91409268a8aeb78
norvik <ai.norvik@gmail.com>
2025-04-10 23:06:46 +0400

    loaded the langfuse credentials and initalize the langfuse client

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from langfuse import Langfuse
 from anthropic import AsyncAnthropic
 
 load_dotenv()
@@ -8,5 +9,9 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
-
 ANTHROPIC_MODEL = AsyncAnthropic(api_key=ANTHROPIC_API_KEY)
+
+LANGFUSE_SECRET_KEY = os.getenv("LANGFUSE_SECRET_KEY")
+LANGFUSE_PUBLIC_KEY = os.getenv("LANGFUSE_PUBLIC_KEY")
+LANGFUSE_HOST = os.getenv("LANGFUSE_HOST")
+langfuse = Langfuse(public_key=LANGFUSE_PUBLIC_KEY, secret_key=LANGFUSE_SECRET_KEY, host=LANGFUSE_HOST)
